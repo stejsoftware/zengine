12123bfb69df8cb7b8621cff1012b10ea9c1b3b4
JONATHAN MEYER <jon@stej.com>
2020-04-29 08:08:22 -0400

    security

---
+++ src/main/java/com/stejsoftware/zengine/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.stejsoftware.zengine.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // @formatter:off
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+                .authorizeRequests(authorize -> authorize
+                    .antMatchers("/cpu", "/cpu/**").permitAll()
+					.antMatchers("/css/**", "/index").permitAll()
+					.antMatchers("/user/**").hasRole("USER")
+				)
+				.formLogin(formLogin -> formLogin
+					.loginPage("/login")
+					.failureUrl("/login-error")
+                );
+	}
+	// @formatter:on
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+            .username("admin")
+            .password("admin")
+            .roles("USER")
+            .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+}
